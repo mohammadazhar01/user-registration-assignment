682b0d7f8125fb598695901c92e11fbcc4bd3606
Mohammad Azhar <mohammadazhar6am@gmail.com>
2025-12-07 08:03:21 +0530

    solve normailer issue

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -27,7 +27,9 @@ export const registerUser = async (req, res)=>{
         otpStore[email] = { otp, expiresAt };
 
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
               user: process.env.EMAIL_USER,
               pass: process.env.EMAIL_PASS
