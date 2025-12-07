18a6e442115736f967453183d23633ef15432bb2
Mohammad Azhar <mohammadazhar6am@gmail.com>
2025-12-07 06:46:33 +0530

    Fix: Cors issue

--- server/server.js
+++ server/server.js
@@ -7,18 +7,17 @@ import userRouter from './route/userRoutes.js';
 
 
 const app = express()
-
-const port = process.env.PORT ||4000;
+const port = process.env.PORT || 4000;
+const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL].filter(Boolean)
 
 await connectDB()
 
-const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL]
-
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
 
+
+
+app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.use('/api/user',userRouter )
 
